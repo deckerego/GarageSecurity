b5f366a8ce9e0af2b9f72f8163485127e1beaeb3
John Ellis <deckerego@gmail.com>
2013-10-25 23:42:01 -0400

    Fire the GPIO pin for the door remote

--- routes.py
+++ routes.py
@@ -7,6 +7,8 @@ import sys
 os.chdir(os.path.dirname(__file__))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
 
+import RPi.GPIO as GPIO
+import time
 from config import configuration
 from bottle import Bottle, error, template
 
@@ -19,3 +21,17 @@ def error(error):
 @application.get('/')
 def show_status():
     return template('index')
+
+@application.post('/<door_idx:int>/door')
+def fire_remote(door_idx):
+	# TODO Currently we ignore the door index, it's here to support multiple doors in the future
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
+
+	GPIO.output(11, GPIO.HIGH)
+	time.sleep(0.05)
+	GPIO.output(11, GPIO.LOW)
+
+	GPIO.cleanup()
+
+	return template('index')
