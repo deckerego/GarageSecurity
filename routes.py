890edd53dbc7a9720623b8e2bb85464442061185
John Ellis <deckerego@gmail.com>
2013-10-30 05:51:03 -0400

    Revert "Switched to Skeleton CSS for reactive design"
    This reverts commit 14dd4567be4e7c3347d3f2648352f98507370559.

--- routes.py
+++ routes.py
@@ -7,7 +7,7 @@ import sys
 os.chdir(os.path.dirname(__file__))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
 
-#import wiringpi
+import wiringpi
 import time
 from config import configuration
 from bottle import Bottle, HTTPResponse, static_file, get, put, request, template
@@ -26,19 +26,14 @@ def send_css(filename):
 def show_status():
     return template('index')
 
-@application.get('/recordings')
-def show_status():
-    return template('recordings')
-
 @application.put('/remote/<button:int>')
 def push_remote_button(button):
 	# TODO Currently we ignore which button is pressed
-	'''
 	io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_SYS)
 	io.pinMode(17, io.OUTPUT)
 	io.digitalWrite(17, io.HIGH)
 
 	time.sleep(1)
 	io.digitalWrite(17, io.LOW)
-    '''
+
 	raise HTTPResponse('{ "pressed": %d }' % button, 200)
--- views/css/README.md
+++
@@ -1,4 +0,0 @@
-Use of Skeleton CSS
-===================
-
-The Skeleton CSS files (base.css, layout.css and skeleton.css) are free to use the open-source MIT license, which is detailed at http://www.opensource.org/licenses/mit-license.php . Skeleton is available at http://www.getskeleton.com/ . Skeleton CSS files are included here as a convenience.
