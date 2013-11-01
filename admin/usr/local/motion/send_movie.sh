926e3db12191721d7b5f4933c8d034588a5757fa
deckerego <deckerego@gmail.com>
2013-10-31 23:53:27 -0400

    Only send a movie if its over 600k

--- admin/usr/local/motion/send_movie.sh
+++ admin/usr/local/motion/send_movie.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo "A motion event was recorded and is attached." | mutt -s "Garage Security System" -a "$1" -- deckerego@gmail.com
+FILE_DESC=$(ls -s "$1")
+FILE_SIZE=${FILE_DESC% *}
+
+if [[ $FILE_SIZE > 600 ]]; then
+    echo "A motion event was recorded and is attached." | mutt -s "Garage Security System" -a "$1" -- deckerego@gmail.com
+fi
 
