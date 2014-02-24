7418bdd85352e86e74ef736c2061a2516ab56878
John Ellis <deckerego@gmail.com>
2014-02-23 20:07:55 -0500

    Trying a circular buffer for video streaming, but Im going to punt on the whole thing

--- app/camera.py
+++ app/camera.py
@@ -14,7 +14,7 @@ class Camera(object):
 
     def __init__(self):
         super(Camera, self).__init__()
-        self.stream = io.BytesIO()
+        #self.stream = io.BytesIO()
         self.start()
 
     def __del__(self):
@@ -45,37 +45,52 @@ class Camera(object):
     # De-installation from Bottle as a plugin
     def close(self):
         logger.info("Closing Camera Connection")
+        self.camera.close()
         
     def start(self):
         logger.info("Opening Camera Connection")
         thread.start_new_thread(self.read_camera, ())
 
     def get_stream(self):
-        self.stream.seek(0)
-        print "Getting stream buffer"
-        return self.stream
+        with self.stream.lock:
+            for frame in self.stream.frames:
+                if frame.header:
+                    self.stream.seek(frame.position)
+                    break
+            output = io.BytesIO()
+            output.write(self.stream.read())
+            output.seek(0)
+            print "Getting stream buffer"
+            return output
 
     def read_camera(self):
-        buffer = io.BytesIO()
+        #buffer = io.BytesIO()
 
-        while True:
-            print "Enter camera loop"
-            try:
-                with picamera.PiCamera() as camera:
-                    print "Getting camera snapshot"
-                    #camera.resolution = (1280, 720)
-                    camera.resolution = (640, 480)
-                    camera.start_preview()
-                    time.sleep(2)
-                    camera.start_recording(buffer, format='mjpeg')
-                    print "Recording camera snapshot"
-                    camera.wait_recording(5)
-                    camera.stop_recording()
-            except:
-                print "Unexpected camera error: %s", sys.exc_info()[0]
-                logger.error("Unexpected camera error: %s", sys.exc_info()[0])
+        print "Enter camera loop"
+        self.camera = picamera.PiCamera()
 
-            self.stream = buffer
+        try:
+            print "Getting camera snapshot"
+            #self.camera.resolution = (1280, 720)
+            self.camera.resolution = (640, 480)
+            self.camera.start_preview()
+            time.sleep(2)
+
+            self.stream = picamera.PiCameraCircularIO(self.camera, seconds=10)
+            self.camera.start_recording(self.stream, format='mjpeg')
+            print "Recording camera snapshot"
+
+            while True:
+                self.camera.wait_recording(5)
+
+        except:
+            print "Unexpected camera error: %s", sys.exc_info()[0]
+            logger.error("Unexpected camera error: %s", sys.exc_info()[0])
+
+        finally:
+            print "Closing camera"
+            self.camera.stop_recording()
+            self.camera.close()
 
 
 class PluginError(Exception):
